1ad61e7a708d72760731c8ec8a5b1752fc1f31e0
jsmolka <jsmolka@pm.me>
2025-12-04 20:52:39 +0100

    generate web images

--- png.sh
+++ png.sh
@@ -4,8 +4,11 @@ setViewBox="select-by-element:svg;object-set-attribute:viewBox,-25 -25 150 150"
 setFill="select-by-element:path;object-set-property:fill,"
 
 mkdir png
-inkscape logo.svg -w 460 -h 460 -b "#242933" --actions="$setViewBox;$setFill #ebedf2" -o png/github.png
 inkscape logo.svg -w 512 -h 512 -b "#242933" --actions="$setViewBox;$setFill #ebedf2" -o png/light-on-dark.png
 inkscape logo.svg -w 512 -h 512              --actions="$setViewBox;$setFill #ebedf2" -o png/light-on-transparent.png
 inkscape logo.svg -w 512 -h 512 -b "#ebedf2" --actions="$setViewBox;$setFill #242933" -o png/dark-on-light.png
 inkscape logo.svg -w 512 -h 512              --actions="$setViewBox;$setFill #242933" -o png/dark-on-transparent.png
+inkscape logo.svg -w 460 -h 460 -b "#242933" --actions="$setViewBox;$setFill #ebedf2" -o png/github.png
+inkscape logo.svg -w 180 -h 180 -b "#242933" --actions="$setViewBox;$setFill #ebedf2" -o png/apple-touch-icon.png
+inkscape logo.svg -w 192 -h 192 -b "#242933" --actions="$setViewBox;$setFill #ebedf2" -o png/android-chrome-192x192.png
+inkscape logo.svg -w 512 -h 512 -b "#242933" --actions="$setViewBox;$setFill #ebedf2" -o png/android-chrome-512x512.png
